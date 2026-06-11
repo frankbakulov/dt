1dd551bf0a708778eb55eedb1d4a3132f1be83cd
Fedor Bakulov <f.bakulov@emicars.ae>
2026-06-11 01:07:07 -0300

    locale

---
+++ locale/en.js
@@ -0,0 +1,21 @@
+export default {
+    weekdays: {
+        shorthand: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+        longhand: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    },
+    months: {
+        shorthand: [null, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        longhand: [null, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        parental: [null, 'January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'],
+    },
+    ruUnits: {
+        'year': 'Y',
+        'calendar day': 'd',
+        'quarter': 'Q',
+        'month': 'M',
+        'week': 'w',
+        'working day': 'workingDay',
+    },
+    rangeSeparator: ' — ',
+};
---
+++ locale/es.js
@@ -0,0 +1,21 @@
+export default {
+    weekdays: {
+        shorthand: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+        longhand: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+    },
+    months: {
+        shorthand: [null, 'ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'],
+        longhand: [null, 'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
+        parental: [null, 'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
+            'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
+    },
+    ruUnits: {
+        'año': 'Y',
+        'día calendario': 'd',
+        'trimestre': 'Q',
+        'mes': 'M',
+        'semana': 'w',
+        'día laborable': 'workingDay',
+    },
+    rangeSeparator: ' — ',
+};
